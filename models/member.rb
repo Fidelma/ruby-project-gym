2aadad89bb4bf3fe7f6e9ed87fa84bc320d1ee70
Fidelma <fidelma.beagan@gmail.com>
2019-07-17 18:07:02 +0100

    update method added for member

--- models/member.rb
+++ models/member.rb
@@ -23,6 +23,24 @@ class Member
     @id = SqlRunner.run(sql, values).first['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE members SET
+    (
+      first_name,
+      last_name
+      ) = ($1, $2)"
+    values = [@first_name, @last_name]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM members
+    WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values).first
+    return Member.new(result)
+  end
+
   def self.all()
     sql = "SELECT * FROM members"
     results = SqlRunner.run(sql)
