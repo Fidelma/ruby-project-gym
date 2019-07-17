5e360690df335d96dacf542a83c262cec4c1304b
Fidelma <fidelma.beagan@gmail.com>
2019-07-17 17:55:59 +0100

    added .all method for members

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,15 @@
-require_relative('')
+require_relative('../models/member')
+require_relative('../models/class')
+require_relative('../models/schedule')
+
+require('pry')
+
+member1 = Member.new({
+  'first_name' => 'Fidelma',
+  'last_name' => 'Beagan'
+  })
+
+  member1.save()
+
+  binding.pry
+  nil
--- models/member.rb
+++ models/member.rb
@@ -11,4 +11,22 @@ class Member
     @last_name = options['last_name']
   end
 
+  def save()
+    sql = "INSERT INTO members
+    (
+      first_name,
+      last_name
+      )
+      VALUES ($1, $2)
+      RETURNING id"
+    values = [@first_name, @last_name]
+    @id = SqlRunner.run(sql, values).first['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM members"
+    results = SqlRunner.run(sql)
+    return results.map { |member| Member.new(member) }
+  end
+
 end
