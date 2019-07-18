b537b5e513147bf43338b8944a8e06bd6988eddc
Fidelma <fidelma.beagan@gmail.com>
2019-07-18 17:24:02 +0100

    edit member view added

--- gym.rb
+++ gym.rb
@@ -1,5 +1,6 @@
 require('sinatra')
 require('sinatra/reloader')
+require('pry')
 require_relative('./models/member.rb')
 require_relative('./models/session.rb')
 require_relative('./models/schedule.rb')
@@ -22,5 +23,21 @@ end
 post '/gym/members' do
   @member = Member.new(params)
   @member.save()
+  redirect to '/gym/members'
+end
+
+get '/gym/members/:id' do
+  @member = Member.find(params[:id])
+  erb(:'members/show')
+end
+
+get '/gym/members/:id/edit' do
+  @member = Member.find(params[:id])
+  erb(:'members/edit')
+end
+
+
+post '/gym/members/:id' do
+  Member.new(params).update
   redirect to '/gym/members'
 end
--- models/member.rb
+++ models/member.rb
@@ -28,8 +28,9 @@ class Member
     (
       first_name,
       last_name
-      ) = ($1, $2)"
-    values = [@first_name, @last_name]
+      ) = ($1, $2)
+      WHERE id = $3"
+    values = [@first_name, @last_name, @id]
     SqlRunner.run(sql, values)
   end
 
