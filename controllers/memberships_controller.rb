afff602f3e03f87a6605b6781f22e4acc3ccccaa
Fidelma <fidelma.beagan@gmail.com>
2019-07-23 17:49:42 +0100

    added namespace for memberships view

--- controllers/memberships_controller.rb
+++ controllers/memberships_controller.rb
@@ -1,5 +1,6 @@
 require('sinatra')
 require('sinatra/reloader')
+require('sinatra/namespace')
 require('pry')
 require_relative('../models/member.rb')
 require_relative('../models/session.rb')
@@ -7,38 +8,40 @@ require_relative('../models/schedule.rb')
 require_relative('../models/membership.rb')
 also_reload('./models/*')
 
+namespace '/gym/memberships' do
 
-get '/gym/memberships' do
-  @memberships = Membership.all()
-  erb(:'memberships/index')
+  get do
+    @memberships = Membership.all()
+    erb(:'memberships/index')
+  end
+
+  get '/new' do
+    erb(:'memberships/new')
+  end
+
+  post do
+    @membership = Membership.new(params)
+    @membership.save()
+    redirect to '/gym/memberships'
+  end
+
+  get '/:id' do
+    @membership = Membership.find(params[:id])
+    erb(:'memberships/show')
+  end
+
+  get '/:id/edit' do
+    @membership = Membership.find(params[:id])
+    erb(:'memberships/edit')
+  end
+
+  post '/:id' do
+    Membership.new(params).update
+    redirect to '/gym/memberships'
+  end
+
+  post '/:id/delete' do
+    Membership.delete(params[:id])
+    redirect to '/gym/memberships'
+  end
 end
-
-get '/gym/memberships/new' do
-  erb(:'memberships/new')
-end
-
-post '/gym/memberships' do
-  @membership = Membership.new(params)
-  @membership.save()
-  redirect to '/gym/memberships'
-end
-
-get '/gym/memberships/:id' do
-  @membership = Membership.find(params[:id])
-  erb(:'memberships/show')
-end
-
-get '/gym/memberships/:id/edit' do
-  @membership = Membership.find(params[:id])
-  erb(:'memberships/edit')
-end
-
-post '/gym/memberships/:id' do
-  Membership.new(params).update
-  redirect to '/gym/memberships'
-end
-
-post '/gym/memberships/:id/delete' do
-  Membership.delete(params[:id])
-  redirect to '/gym/memberships'
-end 
